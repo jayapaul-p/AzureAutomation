3033dd5563ac34a13fe342fd3c51a80701d3cd2d
Jayapaul P <Jayapaul.P@outlook.com>
2019-03-04 22:42:30 +0530

    g

--- OMS-SNOW-Integration/setup.ps1
+++ OMS-SNOW-Integration/setup.ps1
@@ -20,10 +20,14 @@ Param
 
 try {
     New-AzAutomationAccount -Name $AutomationAccountName -ResourceGroupName $ResourceGroupName -Location $ResourceGroupLocation -ErrorAction Stop;
+
+	Write-Output "New automation account has been created successfully";
+
 }
 catch {
 
     Write-Error $_;
+	Write-Output "New automation account creation has been failed.\nError Message: $_.Message";
     exit;
 }
 
@@ -32,9 +36,11 @@ try {
     Set-Location ./AzureAutomation/OMS-SNOW-Integration/Runbook/ -ErrorAction Stop;
     Import-AzAutomationRunbook -Path ./Create-SNOWIncident.ps1 -Type PowerShell -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name Create-SNOWIncident -ErrorAction Stop;
 	Import-AzAutomationRunbook -Path ./Get-SNOWIncPayload.ps1 -Type PowerShell -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Name Get-SNOWIncPayload -ErrorAction Stop;
+	Write-Output "Runbooks have been imported successfully";
 }
 catch {
     Write-Error $_;
+Write-Output "Unable to import runbooks.\nError Message: $_.Message";
     exit
 }
 
@@ -42,34 +48,42 @@ catch {
 try {
     Publish-AzAutomationRunbook -Name Create-SNOWIncident -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -ErrorAction Stop;
 	Publish-AzAutomationRunbook -Name Get-SNOWIncPayload -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -ErrorAction Stop;
+	Write-Output "Runbooks have been published successfully";
 }
 catch {
     Write-Error $_;
+	Write-Output "Unable to publish runbook.\nError Message: $_.Message";
     exit
 }
 
 try {
     New-AzAutomationCredential -Name SNOW-Connection -ResourceGroupName $ResourceGroupName -Value $ServiceNowCredential -AutomationAccountName $AutomationAccountName -ErrorAction Stop;
+Write-Output "SNOW connection has been created successfully";
 }
 catch {
     Write-Host $_;
+	Write-Output "Error occurred while creating SNOW connection.\nError Message: $_.Message";
     exit;
 }
 
 try {
     New-AzAutomationVariable -Name snowInstaceName -Value $ServiceNowInstanceName -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Encrypted $false -ErrorAction Stop;
 	New-AzAutomationVariable -Name snowEndpoint -Value "/api/now/v1/table/incident" -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Encrypted $false -ErrorAction Stop;
+	Write-Output "Automation variables have been created successfully";
 }
 catch {
     Write-Host $_;
+	Write-Output "Error occurred while creating automation variables"
     exit;
 }
 
 try {
     New-AzAutomationWebhook -Name ServiceNow-Incident -ExpiryTime "12/12/2019" -RunbookName "Create-SNOWIncident" -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -IsEnabled $true -Force -ErrorAction Stop;
+Write-Output "Webhook has been created successfully";
 }
 catch {
     Write-Host $_;
+	Write-Output "Error occurred while creating webhook";
     exit;
     
 }
