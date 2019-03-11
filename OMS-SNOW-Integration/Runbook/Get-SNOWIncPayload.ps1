a6110450a601f8080247aace7d503c59114065df
Jayapaul P <jayapaul.p@outlook.com>
2019-03-11 21:07:15 +0530

    Update Get-SNOWIncPayload.ps1

--- OMS-SNOW-Integration/Runbook/Get-SNOWIncPayload.ps1
+++ OMS-SNOW-Integration/Runbook/Get-SNOWIncPayload.ps1
@@ -3,24 +3,24 @@ param (
     $AlertInput
 )
 
-if($AlertInput) {
+if(-not $AlertInput) {
 	Write-Error "Alert inputs were empty" -ErrorAction Stop
 }
 
 $data = ConvertFrom-Json -InputObject $AlertInput
 
-$computerName = $data.data.context.resourceName;
-$alertName = $data.data.context.name;
+$alertRule = $data.AlertRuleName;
+$description = $data.Description;
 
 $input = @{};
-$input.short_description = "$alertName - $computerName" #Short Description
+$input.short_description = "$alertRule" #Short Description
 $input.caller_id = '' #User Id
 $input.category = '' #Inquiry type
 $input.service_offering = '' #Service
 $input.cmdb_ci = '' #Configuration item
 $input.assignment_group ='' #Assignment Group
 $input.priority = '' #Priority
-$input.comments = "$alertName - $computerName"
+$input.comments = "$description"
 
 $body = ConvertTo-Json -InputObject $input;
 
